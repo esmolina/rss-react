2f98946a1212cbad0e33e5650a2519cab6c08bfc
esmolina <elenasmolinann@gmail.com>
2023-03-14 17:46:34 +0300

    feat: add data base

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -3,13 +3,13 @@ import {render, screen} from "@testing-library/react";
 import App from '../App';
 
 describe('App', () => {
-    it('Renders Vite + React', () => {
-        // ARRANGE
-        render(<App/>)
-        // ACT
-        // EXPECT
-        expect(screen.getByRole('heading', {
-            level: 1
-        })).toHaveTextContent('Vite + React');
-    })
+  it('Renders Vite + React', () => {
+    // ARRANGE
+    render(<App/>)
+    // ACT
+    // EXPECT
+    expect(screen.getByRole('heading', {
+      level: 1
+    })).toHaveTextContent('Vite + React');
+  })
 })

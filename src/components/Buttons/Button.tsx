38501a19812f4983c786939c13264d3cad58ef97
esmolina <elenasmolinann@gmail.com>
2023-03-17 13:14:04 +0300

    feat: add button-component

---
+++ src/components/Buttons/Button.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+import { ButtonProps } from './ButtonTypes';
+
+const cx = classNames.bind(styles);
+
+class Button extends Component<ButtonProps> {
+  render(): React.ReactNode {
+    const { buttonText, customClass } = this.props;
+    return (
+      <button type="button" className={cx('button', customClass)}>
+        {buttonText}
+      </button>
+    );
+  }
+}
+
+export default Button;
--- src/components/Page404/Page404.tsx
+++ src/components/Page404/Page404.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
 import styles from './Page404.module.scss';
+import Button from '../Buttons/Button';
 
 const cx = classNames.bind(styles);
 
@@ -9,7 +10,9 @@ class Page404 extends Component<any> {
   render(): React.ReactNode {
     return (
       <div className={cx('page404__container')}>
+        <h1 className={cx('page404__number')}>404</h1>
         <p className={cx('page404__text')}>Oops! This page doesn`t exist..</p>
+        <Button buttonText="Let's go to catalog ;)" customClass="page404" />
       </div>
     );
   }

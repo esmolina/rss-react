ac63d2f904855d8e19d8876eb4754b5edb99a816
esmolina <elenasmolinann@gmail.com>
2023-04-03 00:16:25 +0300

    refactor: rewrite button-component and input-component with hooks

--- src/components/Elements/Buttons/Button.tsx
+++ src/components/Elements/Buttons/Button.tsx
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Button.module.scss';
 import { ButtonProps } from './ButtonTypes';
 
 const cx = classNames.bind(styles);
 
-class Button extends Component<ButtonProps> {
-  noHandler = () => {
+function Button({ buttonType, buttonText, customClass, isSubmit, handleSubmit }: ButtonProps) {
+  const noHandler = () => {
     return 'is not handler';
   };
 
-  render(): React.ReactNode {
-    const { buttonType, buttonText, customClass, isSubmit, handleSubmit } = this.props;
-    const handler = isSubmit ? handleSubmit : this.noHandler;
-    return (
-      // eslint-disable-next-line react/button-has-type
-      <button type={buttonType} className={cx('button', customClass)} onClick={handler}>
-        {buttonText}
-      </button>
-    );
-  }
+  const handler = isSubmit ? handleSubmit : noHandler;
+  return (
+    // eslint-disable-next-line react/button-has-type
+    <button type={buttonType} className={cx('button', customClass)} onClick={handler}>
+      {buttonText}
+    </button>
+  );
 }
 
 export default Button;
--- src/components/Elements/Input/Input.tsx
+++ src/components/Elements/Input/Input.tsx
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Input.module.scss';
 import { InputProps } from './InputTypes';
 
 const cx = classNames.bind(styles);
 
-class Input extends Component<InputProps> {
-  render(): React.ReactNode {
-    const {
-      labelText,
-      labelHtmlFor,
-      customClassLabel,
-      customClassInput,
-      spanClass,
-      inputType,
-      inputValue,
-      spanInfo,
-    } = this.props;
-    return (
-      <div>
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label htmlFor={labelHtmlFor} className={cx('label', customClassLabel)}>
-          {labelText}
-        </label>
-        <input
-          type={inputType}
-          id={labelHtmlFor}
-          value={inputValue}
-          className={cx('input', customClassInput)}
-        />
-        <span className={cx('spanForInput', spanClass)}>{spanInfo}</span>
-      </div>
-    );
-  }
+function Input({
+  labelText,
+  labelHtmlFor,
+  customClassLabel,
+  customClassInput,
+  spanClass,
+  inputType,
+  inputValue,
+  spanInfo,
+}: InputProps) {
+  return (
+    <div>
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label htmlFor={labelHtmlFor} className={cx('label', customClassLabel)}>
+        {labelText}
+      </label>
+      <input
+        type={inputType}
+        id={labelHtmlFor}
+        value={inputValue}
+        className={cx('input', customClassInput)}
+      />
+      <span className={cx('spanForInput', spanClass)}>{spanInfo}</span>
+    </div>
+  );
 }
 
 export default Input;

61b6889c30f309b8d44611a452fc780555cd2969
esmolina <elenasmolinann@gmail.com>
2023-04-03 00:09:59 +0300

    refactor: rewrite catalog-page with hooks

--- src/components/catalog/CardsList/CardsList.tsx
+++ src/components/catalog/CardsList/CardsList.tsx
@@ -7,17 +7,14 @@ import styles from './CardsList.module.scss';
 
 const cx = classNames.bind(styles);
 
-class CardsList extends React.Component<CardsListProps> {
-  render() {
-    const { productsData } = this.props;
-    return (
-      <div data-testid="test-cards-list" className={cx('catalog-grid')}>
-        {productsData.map((product: ProductInterface) => {
-          return <LittleCard product={product} key={product.productCode} />;
-        })}
-      </div>
-    );
-  }
+function CardsList({ productsData }: CardsListProps) {
+  return (
+    <div data-testid="test-cards-list" className={cx('catalog-grid')}>
+      {productsData.map((product: ProductInterface) => {
+        return <LittleCard product={product} key={product.productCode} />;
+      })}
+    </div>
+  );
 }
 
 export default CardsList;
--- src/components/catalog/CatalogWrapper/CatalogWrapper.tsx
+++ src/components/catalog/CatalogWrapper/CatalogWrapper.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classNames from 'classnames/bind';
 import { CatalogWrapperProps } from './CatalogWrapperTypes';
 import styles from './CatalogWraper.module.scss';
@@ -7,21 +7,17 @@ import CardsList from '../CardsList/CardsList';
 
 const cx = classNames.bind(styles);
 
-class CatalogWrapper extends React.Component<CatalogWrapperProps> {
-  componentDidMount() {
-    // eslint-disable-next-line react/destructuring-assignment
-    this.props.handleGoAnotherChange('Catalog');
-  }
+function CatalogWrapper({ productsData, handleGoAnotherChange }: CatalogWrapperProps) {
+  useEffect(() => {
+    handleGoAnotherChange('Catalog');
+  });
 
-  render() {
-    const { productsData } = this.props;
-    return (
-      <div className={cx('catalog-wrapper')}>
-        <Searcher />
-        <CardsList productsData={productsData} />
-      </div>
-    );
-  }
+  return (
+    <div className={cx('catalog-wrapper')}>
+      <Searcher />
+      <CardsList productsData={productsData} />
+    </div>
+  );
 }
 
 export default CatalogWrapper;

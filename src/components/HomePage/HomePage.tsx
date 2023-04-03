5b29394d3291c72ab97fee4fb30c525f4b6fa916
esmolina <elenasmolinann@gmail.com>
2023-04-03 13:13:21 +0300

    refactor: rewrite home-page with hooks

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames/bind';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './HomePage.module.scss';
 import Button from '../Elements/Buttons/Button';
@@ -7,27 +7,24 @@ import { HomePageProps } from './HomePagTypes';
 
 const cx = classNames.bind(styles);
 
-class About extends Component<HomePageProps> {
-  componentDidMount() {
-    // eslint-disable-next-line react/destructuring-assignment
-    this.props.handleGoAnotherChange('Home');
-  }
+function About({ handleGoAnotherChange }: HomePageProps) {
+  useEffect(() => {
+    handleGoAnotherChange('Home');
+  });
 
-  render(): React.ReactNode {
-    return (
-      <div className={cx('home-page')}>
-        <h1 className={cx('home-page__header')}>Beauty Land</h1>
-        <Link to="/catalog" className={cx('home-page__router-link')}>
-          <Button
-            buttonType="button"
-            buttonText="Go to Catalog"
-            customClass="home-page__button"
-            isSubmit={false}
-          />
-        </Link>
-      </div>
-    );
-  }
+  return (
+    <div className={cx('home-page')}>
+      <h1 className={cx('home-page__header')}>Beauty Land</h1>
+      <Link to="/catalog" className={cx('home-page__router-link')}>
+        <Button
+          buttonType="button"
+          buttonText="Go to Catalog"
+          customClass="home-page__button"
+          isSubmit={false}
+        />
+      </Link>
+    </div>
+  );
 }
 
 export default About;

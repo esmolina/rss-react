f4d399e0d433a80cb9d14be0091d9b80134054e4
esmolina <elenasmolinann@gmail.com>
2023-04-04 14:42:33 +0300

    feat: add form tests

--- src/test/Forms.test.tsx
+++ src/test/Forms.test.tsx
@@ -1,25 +1,45 @@
-import { describe } from 'vitest';
-import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { FeedbackCardProps } from '../components/Feedback/FeedbackCard/FeedbackCardTypes';
 
 import '@testing-library/jest-dom/extend-expect';
 import QuizForm from '../components/Feedback/QuizForm/QuizForm';
+import FeedbackCard from '../components/Feedback/FeedbackCard/FeedbackCard';
 
-// const testNewFeedback = {
-//   userName: 'Elena',
-//   product: 'Solgar - Omega3',
-//   datePurchase: '01.01.2023',
-//   opinion: 'good',
-//   file: 'img.jpg',
-// };
+const testNewFeedback = {
+  userName: 'Elena',
+  product: 'Solgar - Omega3',
+  datePurchase: '01.01.2023',
+  opinion: 'good',
+  file: 'img.jpg',
+};
 
 const testHandleNewFeedbackFieldsRender = (newFeedback: FeedbackCardProps) => {
   const feedbacks: Array<FeedbackCardProps> = [];
   feedbacks.push(newFeedback);
 };
-describe('Form tests', () => {
-  test('render form', () => {
+describe('Form fields rendering', () => {
+  test('render form fields', () => {
     render(<QuizForm handleNewFeedback={testHandleNewFeedbackFieldsRender} />);
+    expect(screen.getByLabelText('My name...')).toBeInTheDocument();
+    expect(screen.getByLabelText('Date of purchase...')).toBeInTheDocument();
+    expect(screen.getByLabelText('Good')).toBeInTheDocument();
+    expect(screen.getByLabelText('Bad')).toBeInTheDocument();
+  });
+});
+
+describe('Feedback-card rendering', () => {
+  it('renders right card', () => {
+    render(
+      <FeedbackCard
+        userName={testNewFeedback.userName}
+        product={testNewFeedback.product}
+        datePurchase={testNewFeedback.datePurchase}
+        opinion={testNewFeedback.opinion}
+        file={testNewFeedback.file}
+      />
+    );
+    expect(screen.getByText(/Elena/)).toBeInTheDocument();
   });
 });

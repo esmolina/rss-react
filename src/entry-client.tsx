bdce536f16300ecb4b382f157138469d17d5cfd3
esmolina <elenasmolinann@gmail.com>
2023-05-01 10:40:56 +0300

    refactor: change config, fix entry-client

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -2,5 +2,17 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.hydrateRoot(document.getElementById('root') as HTMLElement, <App />);
+ReactDOM.hydrateRoot(
+  document.getElementById('root') as HTMLElement,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);

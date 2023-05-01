dd70fb17f4d08704ad377d0f562b53424efc5bab
esmolina <elenasmolinann@gmail.com>
2023-05-01 10:27:27 +0300

    feat: add server

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import App from './App';
-import HTMLWrapper from './components/HTMLWrapper';
 import { Response } from 'express';
+import HtmlWrapper from './components/HtmlWrapper';
 
 export function render(response: Response) {
   // eslint-disable-next-line react/no-children-prop
-  const { pipe } = ReactDOMServer.renderToPipeableStream(<HTMLWrapper children={<App />} />, {
+  const { pipe } = ReactDOMServer.renderToPipeableStream(<HtmlWrapper children={<App />} />, {
     // bootstrapScripts: ['/entry-client.tsx'],
     onShellReady() {
       response.setHeader('content-type', 'text/html');
